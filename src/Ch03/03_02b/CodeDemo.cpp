8f4cfea79afb8e7996c935e3d417d50ac2e0c3b8
Lambert Nguyen <151005054+Lambert-Nguyen@users.noreply.github.com>
2025-03-22 03:15:07 +0000

    class, and using classes

--- src/Ch03/03_02b/CodeDemo.cpp
+++ src/Ch03/03_02b/CodeDemo.cpp
@@ -7,19 +7,37 @@
 
 enum class cow_purpose {dairy, meat, hide, pet};
 
-struct cow{
+class cow{
+private:
     std::string name;
     int age;
     cow_purpose purpose;
+public:
+    std::string getName()const{
+        return name;
+    }
+    int getAge() const{
+        return age;
+    }
+    cow_purpose get_purpose() const {
+        return purpose;
+    }
+
+    void set_age(int new_age){
+        age = new_age;
+    }
+
+    cow(std::string name_i, int age_i, cow_purpose purpose_i){
+        name = name_i;
+        age = age_i;
+        purpose = purpose_i;
+    }
 };
 
 int main(){
-    cow my_cow;
-    my_cow.age = 5;
-    my_cow.name = "Betsy";
-    my_cow.purpose = cow_purpose::dairy;
-    std::cout << my_cow.name << " is a type-" << (int)my_cow.purpose << " cow." << std::endl;
-    std::cout << my_cow.name << " is " << my_cow.age << " years old." << std::endl;
+    cow my_cow("Hildy", 7, cow_purpose::pet);
+    std::cout << my_cow.getName() << " is a type-" << (int)my_cow.get_purpose() << " cow." << std::endl;
+    std::cout << my_cow.getName() << " is " << my_cow.getAge() << " years old." << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
